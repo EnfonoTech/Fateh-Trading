661f0f5c7689ed41f8e5da9cd2b2f0672e3b9d07
AravindR97 <aravindrajeshchry@gmail.com>
2026-01-20 11:42:26 +0530

    fix(Discount On Amount): Reset discount on changing rate

--- fateh_trading/public/js/purchase_invoice.js
+++ fateh_trading/public/js/purchase_invoice.js
@@ -8,9 +8,12 @@ frappe.ui.form.on("Purchase Invoice Item", {
     rate(frm, cdt, cdn) {
         const row = locals[cdt][cdn];
 
-        if (!row.custom_actual_rate) {
-            frappe.model.set_value(cdt, cdn, "custom_actual_rate", row.rate);
+        // Skip if this is a programmatic change
+        if (row.__updating_rate_programmatically) {
+            return;
         }
+
+        frappe.model.set_value(cdt, cdn, "custom_discount_on_amount", null);
     },
 
     item_code(frm, cdt, cdn) {
@@ -27,7 +30,16 @@ frappe.ui.form.on("Purchase Invoice Item", {
         if (row.custom_discount_on_amount) {
             const discount_per_unit = row.custom_discount_on_amount / row.qty;
             const new_rate = row.rate - discount_per_unit;
+            
+            // Set flag before programmatic update
+            row.__updating_rate_programmatically = true;
+            
             frappe.model.set_value(cdt, cdn, "rate", new_rate);
+            
+            // Clean up flag after a short delay to ensure the rate event has processed
+            setTimeout(() => {
+                delete row.__updating_rate_programmatically;
+            }, 100);
         }
     }
 });
